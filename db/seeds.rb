e435dad07d5149743b2d8ad10876ed72f3351919
Harm Binnenkade <harmbinnenkade@gmail.com>
2021-02-17 17:03:21 +0100

    continue from create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mac_donalds = { name: 'Mc Donalds', address: 'Albert Cuypstraat', phone_number: '123456789', category: 'french' }
+ratatouille = { name: 'Ratatouille', address: 'Paris', phone_number: '123456789', category: 'chinese' }
+little_saigon = { name: 'Little Saigon', address: 'Amsterdam-Noord', phone_number: '123456789', category: 'japanese' }
+franks_smokehouse = { name: "Frank's Smokehouse", address: 'Amsterdam-Oost', phone_number: '123456789',
+                      category: 'italian' }
+loetje = { name: 'Loetje', address: 'Amsterdam-Zuid', phone_number: '123456789', category: 'belgian' }
+
+[mac_donalds, ratatouille, little_saigon, franks_smokehouse, loetje].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
